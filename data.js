a0d5888863abb7a93be7a4ab08cfe8b6aa4c8e17
MRutkowski00 <m.rutkowski.archviz@gmail.com>
2024-03-22 11:57:41 +0100

    Upload

---
+++ data.js
@@ -0,0 +1,419 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01_recepcja_a_post",
+      "name": "01_Recepcja_A_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9745139624274763,
+          "pitch": -0.020655536441296363,
+          "rotation": 0,
+          "target": "2-02_s_konferencyjna_post"
+        },
+        {
+          "yaw": 1.2564452855219148,
+          "pitch": -0.007394964131702153,
+          "rotation": 0,
+          "target": "1-01_recepcja_b_post"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-01_recepcja_b_post",
+      "name": "01_Recepcja_B_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.037045017445311856,
+          "pitch": -0.030282465076815868,
+          "rotation": 0,
+          "target": "4-s_komunikacja_05_a_post"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-02_s_konferencyjna_post",
+      "name": "02_S_konferencyjna_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2000685874885413,
+          "pitch": -0.01557913531472721,
+          "rotation": 0,
+          "target": "0-01_recepcja_a_post"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-03_wc_ozn_post",
+      "name": "03_WC_OzN_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-s_komunikacja_05_a_post",
+      "name": "S_Komunikacja_05_A_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.005143387522341669,
+          "pitch": 0.03341943411538395,
+          "rotation": 0,
+          "target": "5-s_komunikacja_05_c_post"
+        },
+        {
+          "yaw": 0.2102967514548677,
+          "pitch": 0.03017164016903351,
+          "rotation": 0,
+          "target": "10-s_s_konferencyjna_08_a_post"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-s_komunikacja_05_c_post",
+      "name": "S_Komunikacja_05_C_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5587234029314985,
+          "pitch": 0.14076557455295458,
+          "rotation": 0,
+          "target": "9-s_s_konferencyjna_07_a_post"
+        },
+        {
+          "yaw": -0.007714995779132039,
+          "pitch": 0.02057021741915044,
+          "rotation": 0,
+          "target": "4-s_komunikacja_05_a_post"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-04_open_space_a_post",
+      "name": "04_Open_Space_A_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-04_open_space_b_post",
+      "name": "04_Open_Space_B_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-s_kantyna_06_a_post",
+      "name": "S_Kantyna_06_A_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-s_s_konferencyjna_07_a_post",
+      "name": "S_S_konferencyjna_07_A_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6240086047863151,
+          "pitch": 0.03166234017107428,
+          "rotation": 0,
+          "target": "5-s_komunikacja_05_c_post"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-s_s_konferencyjna_08_a_post",
+      "name": "S_S_konferencyjna_08_A_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0076168118624977,
+          "pitch": 0.03264088270101695,
+          "rotation": 0,
+          "target": "4-s_komunikacja_05_a_post"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Wykroty_0_VR_D",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
